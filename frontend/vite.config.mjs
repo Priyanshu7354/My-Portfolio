6fd259ce00e463824555295c4f683c37a90a748d
Priyanshu Bhatnagar <166941540+Priyanshu7354@users.noreply.github.com>
2026-03-10 18:03:04 +0530

    fix github pages base path

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // correct for Vercel
+  base: "/My-Portfolio/",   // ✅ change this
   server: {
     proxy: {
       "/api": {
