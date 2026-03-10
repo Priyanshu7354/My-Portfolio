f315f8f29ec0e4b050dc062015325522462321e6
Priyanshu Bhatnagar <166941540+Priyanshu7354@users.noreply.github.com>
2026-03-10 16:39:09 +0530

    fix vite base path for vercel

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/My-Portfolio/",   // repo name
+  base: "/",   // correct for Vercel
   server: {
     proxy: {
       "/api": {
