4d833eabe23cf3a901009261ae2fb9d4e8d9dd79
Priyanshu Bhatnagar <166941540+Priyanshu7354@users.noreply.github.com>
2026-03-10 18:12:10 +0530

    fix gh-pages deployment

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/My-Portfolio/",   // ✅ change this
+  base: "/",   // ✅ change this
   server: {
     proxy: {
       "/api": {
